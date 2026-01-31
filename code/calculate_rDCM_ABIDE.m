43a33d7c6ea8009ce3c70b8653a469eac96b80c3
Xue-Ru Fan <xueru@mail.bnu.edu.cn>
2026-01-31 21:12:45 +0100

    Update calculate_rDCM_ABIDE.m

--- code/calculate_rDCM_ABIDE.m
+++ code/calculate_rDCM_ABIDE.m
@@ -1,7 +1,7 @@
 % =========================================================
 % rDCM analysis for ABIDE ROI time series
 % Site-specific TR handling
-% Classic rDCM (no sparsity)
+% Classic rDCM and rDCM with sparsity
 % 用tapas工具包，计算每个被试、每个run的有效连接矩阵EC，没有进行sparsity约束
 % 有没有 sparsity 的 rDCM，本质区别在于：是否“先验上相信大多数脑区之间是没有直接因果影响的”。
 % 无 sparsity 的 rDCM 假设 所有脑区之间都可能存在有效连接
@@ -14,11 +14,6 @@ clear; clc;
 %% -------------------- paths -----------------------------
 data_root = '/Volumes/Zuolab_XRF/data/abide/';
 ts_path   = fullfile(data_root, 'timeseries');
-out_path  = fullfile(data_root, 'rDCM');
-
-if ~exist(out_path, 'dir')
-    mkdir(out_path);
-end
 
 addpath(genpath('/Users/xuerufan/matlab-toolbox/tapas'));
 
@@ -37,7 +32,14 @@ nFiles = numel(files);
 
 % fprintf('Found %d time series files\n', nFiles);
 
-%% -------------------- main loop -------------------------
+%% -------------------- without sparsity constraints -------------------------
+
+out_path  = fullfile(data_root, 'rDCM');
+
+if ~exist(out_path, 'dir')
+    mkdir(out_path);
+end
+
 for i = 1:nFiles
 
     % load data
@@ -61,6 +63,7 @@ for i = 1:nFiles
 
     DCM = tapas_rdcm_model_specification(Y, [], []);
     rDCM = tapas_rdcm_estimate(DCM, 'r', [], 1);
+%     rDCM variant: 1 = original, 2 = with sparsity constraints
 
     EC = rDCM.Ep.A;
 
@@ -69,4 +72,46 @@ for i = 1:nFiles
 
 end
 
-fprintf('Done!\n');
+fprintf('rDCM Done!\n');
+
+%% -------------------- with sparsity constraints -------------------------
+
+out_path  = fullfile(data_root, 'srDCM');
+
+if ~exist(out_path, 'dir')
+    mkdir(out_path);
+end
+
+for i = 1:nFiles
+
+    % load data
+    fn = files(i).name;
+    load(fullfile(ts_path, fn));   % ROI_ts, siteName, subName
+
+    fprintf('Processing subject %s (site %s)\n', subName, siteName);
+
+    % sanity checks
+    if size(ROI_ts, 1) ~= nROI
+        error('ROI number mismatch in %s', fn);
+    end
+
+    if ~isKey(TR_map, siteName)
+        error('Unknown siteName "%s" in %s', siteName, fn);
+    end
+
+    TR = TR_map(siteName);
+    Y.y  = ROI_ts';     % [T x ROI]
+    Y.dt = TR;
+
+    DCM = tapas_rdcm_model_specification(Y, [], []);
+    rDCM = tapas_rdcm_estimate(DCM, 'r', [], 2);
+%     rDCM variant: 1 = original, 2 = with sparsity constraints
+
+    EC = rDCM.Ep.A;
+
+    out_fn = sprintf('%s_DU15_srDCM.mat', subName);
+    save(fullfile(out_path, out_fn),'EC', 'siteName', 'subName', 'TR');
+
+end
+
+fprintf('srDCM Done!\n');
